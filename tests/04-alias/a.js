6da89e1676d8f59010c70d4108d1a4fc0a2db8db
Shan He <heshan0131@gmail.com>
2021-03-16 22:09:53 -0700

    initial commit
    Signed-off-by: Shan He <heshan0131@gmail.com>

---
+++ tests/04-alias/a.js
@@ -0,0 +1,12 @@
+
+// TYPE IMPORTS
+/** @typedef {import('./a').Foo} Foo */
+/** @typedef {import('./b').Blue} Blue */
+
+/**
+ * set join display property
+ * @type {typeof import('./a').convert}
+ */
+export function convert(a) {
+  return String(a);
+}
