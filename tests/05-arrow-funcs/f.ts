e7e9dd4e31e75086f83f120b607ef0d300775b57
Shan He <heshan0131@gmail.com>
2021-03-18 19:58:07 -0700

    fix tests
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- tests/05-arrow-funcs/f.ts
+++ tests/05-arrow-funcs/f.ts
@@ -1,9 +1,8 @@
 import Action from 'redux-actions';
-
 export type State = {
-  a: string;
+  a: string
 };
-export type Payload = string
+export type Payload = string;
 
 export const createMapUpdater = (state: State, action: Action<Payload>): State => {
   return state;
